8e92a06773d318edae24ba13954ce33506d92c90
Jason Lewicki <lewicki.jason@gmail.com>
2015-03-19 18:10:12 +0000

    Added an autoloader for Core and Exchange classes.

---
+++ PHP5/autoload.php
@@ -0,0 +1,27 @@
+<?php
+// Specify the extensions that may be loaded
+spl_autoload_extensions('.php');
+
+/**
+ * The Autoloader function
+ *
+ * @param string $class_name The Class Name
+ *
+ * @access public
+ */
+spl_autoload_register(function ($class_name) {
+	// Only try to load classes with the WAID namepsace prefix
+	if(strpos($class_name, 'CryptoTraderHub\\') === 0){
+			
+		// Replace namespace separator with directory separator (prolly not required)
+		$class_name = str_replace('\\', DIRECTORY_SEPARATOR, substr($class_name, 21));
+	
+		// Get full name of file containing the required class
+		$file = CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.$class_name.".php";	
+		
+		// Load file if it exists
+		if (is_readable($file)){
+			require_once $file;		
+		}
+	}
+});
