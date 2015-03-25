6b06b743f7bfdc0fb896aaf99dae4ae6ba49ff29
Jason Lewicki <lewicki.jason@gmail.com>
2015-03-25 03:52:31 +0000

    More Test Exchange functionality.

--- PHP5/Exchanges/Test.php
+++ PHP5/Exchanges/Test.php
@@ -16,18 +16,21 @@ class Test implements \CryptoTraderHub\Exchanges\Exchange{
 	
 	private $balance_usd;	
 	private $balance_btc;
+
+	private $transaction_arr;
 	
 	// Constructor
 	public function __construct($test_ini) {
-		$settings 			= parse_ini_file($test_ini);
-		$this->database		= $settings['database'];
-		$this->table 		= $settings['table'];
-		$this->time_start 	= $settings['time_start'];
-		$this->time_end		= $settings['time_end'];	
-		$this->balance_usd 	= $settings['balance_usd'];	
-		$this->balance_btc 	= $settings['balance_btc'];	
-		$this->time_index 	= 0;	
-				
+		$settings 				= parse_ini_file($test_ini);
+		$this->database			= $settings['database'];
+		$this->table 			= $settings['table'];
+		$this->time_start 		= $settings['time_start'];
+		$this->time_end			= $settings['time_end'];	
+		$this->balance_usd 		= $settings['balance_usd'];	
+		$this->balance_btc 		= $settings['balance_btc'];	
+		$this->time_index 		= 0;	
+		$this->transaction_arr 	= Array();		
+	
 		$market_data = \CryptoTraderHub\Core\Database::getArray("SELECT * FROM {$this->database}.{$this->table} WHERE time > {$this->time_start} AND time < {$this->time_end};");	
 	}
 	
@@ -39,15 +42,22 @@ class Test implements \CryptoTraderHub\Exchanges\Exchange{
 	public function testPrivate(){}	
 	
 	// Public (no auth)
-	public function ticker(){}
+	public function ticker(){
+		return $this->market_data[$this->time_index];
+	}
 	public function orderBook(){}
-	public function transactions($timeframe){}
+	
+	public function transactions($timeframe){
+		//return $this->transaction_arr;
+	}
 	
 	// Private (auth required)
 	public function balance(){
 		return Array('usd' => $this->balance_usd,'btc' => $this->balance_btc);
 	}
-	public function userTransactions($limit, $offset){}
+	public function userTransactions($limit, $offset){
+		//return $this->transaction_arr;		
+	}
 	public function openOrders(){}
 	public function cancelOrder($id){}
 	public function buy($amount, $price){}
