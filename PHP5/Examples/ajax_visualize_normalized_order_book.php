e02e47609f62e2ba8d3f8a5d15b524d0d34e1f48
Jason Lewicki <lewicki.jason@gmail.com>
2015-07-20 20:36:24 +0000

    Plotting the order book.

---
+++ PHP5/Examples/ajax_visualize_normalized_order_book.php
@@ -0,0 +1,92 @@
+<?php 
+// Detect if request was sent from JQuery or not
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+		
+	include dirname(__FILE__).'/Configs/config.php';
+	include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
+	\CryptoTraderHub\Core\Database::initialize(DATABASE_INI);
+	$timestamp = $_GET['timestamp'];
+	$result  = \CryptoTraderHub\Core\Database::getArray("SELECT timestamp, price, volume FROM ".\CryptoTraderHub\Core\Database::getDB().".bitstamp_order_book WHERE `timestamp` = (SELECT `timestamp` FROM ".\CryptoTraderHub\Core\Database::getDB().".bitstamp_order_book WHERE `timestamp` > '{$timestamp}' LIMIT 1) AND volume < 1000");
+	$response = Array('timestamp' => $result[0]['timestamp'], 'data_set' => $result);
+	echo json_encode($response);
+	exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		
+		<title>Bitstamp Order Book Visualizer</title>
+		
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<!--[if gte IE 9]>
+		  <style type="text/css">
+		    .gradient {
+		       filter: none;
+		    }
+		  </style>
+		<![endif]-->		
+		
+		<style></style>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>	
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/jquery.canvasjs.min.js"></script>	
+		<script type="text/javascript">
+			
+			window.onload = function () {
+
+		      	var dps = [];   //dataPoints. 
+		
+		      	var chart = new CanvasJS.Chart("chartContainer",{
+		      		title :{
+		      			text: "Order Book Data"
+		      		},
+		      		axisX: {						
+		      			title: "Price"
+		      		},
+		      		axisY: {						
+		      			title: "Volume"
+		      		},
+		      		data: [{
+		      			type: "scatter",
+		      			dataPoints : dps
+		      		}]
+		      	});
+		
+		      	chart.render();
+		      	var xVal = dps.length + 1;
+		      	var yVal = 15;	
+		      	var updateInterval = 1000;
+		      	var timestamp = "1977-01-01 00:00:00";
+		
+		      	var updateChart = function () {	      	
+		      		
+		      		$.ajax({
+					  	url: "/",
+					  	context: document.body,
+					  	dataType: 'json',
+					  	data: {'timestamp':timestamp}
+					}).done(function(data) {
+						timestamp = data.timestamp;
+						dps.length = 0;
+						$.each(data.data_set, function( index, value ) {
+						  	dps.push({x:parseFloat(value.price),y:parseFloat(value.volume)});
+						});					
+			      		chart.render();
+					});		      	
+		      				
+				};
+
+				//setInterval(function(){updateChart()}, updateInterval); 
+			}		
+		</script>
+
+	</head>
+	<body>	
+		<div id="chartContainer" style="height: 300px; width:100%;"></div>
+	</body>
+</html>
--- PHP5/Examples/bitstamp_parse_order_book.php
+++ PHP5/Examples/bitstamp_parse_order_book.php
@@ -15,10 +15,18 @@ $order_book = $bitstamp_obj->orderBook();
 
 // Create SQL statement
 $timestamp = $order_book['timestamp'];
-$sql = '';
+$bids_sql = '';
 foreach($order_book['bids'] as $bid){		
-	$sql .= "('".date('Y-m-d H:i:s', $timestamp)."',{$bid[0]},{$bid[1]}),";					
+	$bids_sql .= "('".date('Y-m-d H:i:s', $timestamp)."','bid',{$bid[0]},{$bid[1]}),";					
+}
+
+$asks_sql = '';
+foreach($order_book['asks'] as $ask){		
+	$asks_sql .= "('".date('Y-m-d H:i:s', $timestamp)."','ask',{$ask[0]},{$ask[1]}),";					
 }
 
 // Insert the order book into the database
-\CryptoTraderHub\Core\Database::runQuery("INSERT INTO ".\CryptoTraderHub\Core\Database::getDB().".bitstamp_order_book (`timestamp`,`price`,`volume`) VALUES  " . rtrim($sql,','). ";");
+\CryptoTraderHub\Core\Database::runQuery("INSERT INTO ".\CryptoTraderHub\Core\Database::getDB().".bitstamp_order_book (`timestamp`,`type`,`price`,`volume`) VALUES  " . rtrim($bids_sql,','). ";");
+
+// Insert the order book into the database
+\CryptoTraderHub\Core\Database::runQuery("INSERT INTO ".\CryptoTraderHub\Core\Database::getDB().".bitstamp_order_book (`timestamp`,`type`,`price`,`volume`) VALUES  " . rtrim($asks_sql,','). ";");
