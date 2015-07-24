818f6d055c7fd0dad88f4bf6836923e2a79199ab
Jason Lewicki <lewicki.jason@gmail.com>
2015-07-24 18:55:35 +0000

    More test exmaples.

---
+++ PHP5/Examples/sell.php
@@ -0,0 +1,9 @@
+<?php
+
+include dirname(__FILE__).'/Configs/config.php';
+include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
+
+// Create Bitstamp object using bitstamp credentials
+$bitstamp_obj = new \CryptoTraderHub\Exchanges\Bitstamp(APP_ROOT.'/Configs/exchange_bitstamp.ini');
+
+// Sell
