3888161177dd32365ef67d1cdb07fb48ef05a3b7
Jason Lewicki <lewicki.jason@gmail.com>
2015-08-10 13:23:50 +0000

    Fromatting.

--- PHP5/Examples/bitstamp_parse_order_book.php
+++ PHP5/Examples/bitstamp_parse_order_book.php
@@ -6,7 +6,6 @@ include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
 // Create Bitstamp object using bitstamp credentials
 $bitstamp_obj = new \CryptoTraderHub\Exchanges\Bitstamp(APP_ROOT.'/Configs/exchange_bitstamp.ini');
 
-
 // Get Bitstamp historical market data and insert into the DB
 \CryptoTraderHub\Core\Database::initialize(DATABASE_INI);
 
