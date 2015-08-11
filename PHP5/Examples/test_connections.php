53cd4ba79685c0a9529df3716e1a093bdc100c7f
Jason Lewicki <lewicki.jason@gmail.com>
2015-08-11 18:04:18 +0000

    Documentation.

--- PHP5/Examples/test_connections.php
+++ PHP5/Examples/test_connections.php
@@ -6,7 +6,7 @@ include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
 // Create Bitstamp object using bitstamp credentials
 $bitstamp_obj = new \CryptoTraderHub\Exchanges\Bitstamp(APP_ROOT.'/Configs/exchange_bitstamp.ini');
 
-// Bitstamp Tests
+// Bitstamp Tests for public and private access
 try{
 	$bitstamp_obj->testPublic();
 	$bitstamp_obj->testPrivate();	
