1f247e8864df335673209d5d9c80beb0754890ec
Jason Lewicki <lewicki.jason@gmail.com>
2016-04-07 15:54:08 +0000

    INI fix

--- PHP5/Examples/simple_algorithm.php
+++ PHP5/Examples/simple_algorithm.php
@@ -5,7 +5,7 @@ include dirname(__FILE__).'/../Configs/config.php';
 include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
 
 // Create a Test exchange with the Historical data that we just inserted into the DB
-$test_exchange_obj = new \CryptoTraderHub\Exchanges\Test(APP_ROOT.'/Configs/historical_bitstampUSD.ini');
+$test_exchange_obj = new \CryptoTraderHub\Exchanges\Test(APP_ROOT.'Examples/Configs/historical_bitstampUSD.ini');
 
 // Variables
 $last_60 			= Array();
