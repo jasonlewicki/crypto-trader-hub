125a4f10d54a4c1dbc5bf56f6569fc362486fccd
Jason Lewicki <lewicki.jason@gmail.com>
2016-04-25 13:18:21 +0000

    Documentation.

--- PHP5/Examples/test_connections.php
+++ PHP5/Examples/test_connections.php
@@ -10,9 +10,14 @@ echo "Testing bitstamp API\n----------------------\n";
 
 // Bitstamp Tests for public and private access
 try{
-	$bitstamp_obj->testPublic();
-	$bitstamp_obj->testPrivate();	
-	echo "passed\n";
+	$bitstamp_obj->testPublic();	
+	echo "Passed public tests.\n";
 }catch (\Exception $e) {
-	echo "error\n";
+	echo "Public tests error.\n";
+}
+try{
+	$bitstamp_obj->testPrivate();	
+	echo "Passed private tests.\n";
+}catch (\Exception $e) {
+	echo "Private tests error.\n";
 }
