005761200cc0f2a66089522744aa261cc2620331
Jason Lewicki <lewicki.jason@gmail.com>
2016-04-26 13:20:46 +0000

    Documentation.

--- PHP5/Examples/test_connections.php
+++ PHP5/Examples/test_connections.php
@@ -8,13 +8,15 @@ $bitstamp_obj = new \CryptoTraderHub\Exchanges\Bitstamp(APP_ROOT.'/Configs/excha
 
 echo "Testing bitstamp API\n----------------------\n";
 
-// Bitstamp Tests for public and private access
+// Bitstamp Tests for public access
 try{
 	$bitstamp_obj->testPublic();	
 	echo "Passed public tests.\n";
 }catch (\Exception $e) {
 	echo "Public tests error.\n";
 }
+
+// Bitstamp Tests for private access
 try{
 	$bitstamp_obj->testPrivate();	
 	echo "Passed private tests.\n";
