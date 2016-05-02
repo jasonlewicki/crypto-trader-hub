2631a2eadfc6ee1ef562de0d8aa19d899736d923
Jason Lewicki <lewicki.jason@gmail.com>
2016-05-02 20:57:46 +0000

    Documentation.

--- PHP5/Examples/bitcoincharts_bitstamp_historical_market_data.php
+++ PHP5/Examples/bitcoincharts_bitstamp_historical_market_data.php
@@ -1,9 +1,12 @@
 <?php
 
+/*
+ * Get Bitstamp historical market data and insert into the DB
+ */
+
 include dirname(__FILE__).'/Configs/config.php';
 include CRYPTO_TRADER_HUB_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
 
-// Get Bitstamp historical market data and insert into the DB
 $bitcoin_charts_obj = new \CryptoTraderHub\DataServices\Bitcoincharts();
 $filename = $bitcoin_charts_obj -> completeHistory('bitstampUSD', true);
 
