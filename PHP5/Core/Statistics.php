c69aef8ee43619e9f3fb954d364000f3f30bbc18
Jason Lewicki <lewicki.jason@gmail.com>
2016-05-16 16:22:44 +0000

    Documentation.

--- PHP5/Core/Statistics.php
+++ PHP5/Core/Statistics.php
@@ -4,7 +4,7 @@ namespace CryptoTraderHub\Core;
 
 class Statistics {
 	
-	// Calculate the standard deviation of the suplpied array
+	// Calculate the standard deviation of the supplied array
     public static function standardDeviation($values_arr){
 		$mean = array_sum($values_arr) / count($values_arr);
 	    $variance = 0.0;
@@ -15,6 +15,7 @@ class Statistics {
 	    return (float) sqrt($variance);
     }
 	
+	// Calculate the mean of the supplied array
 	public static function mean($values_arr){
 		return (float) (array_sum($values_arr) / count($values_arr));
     }
